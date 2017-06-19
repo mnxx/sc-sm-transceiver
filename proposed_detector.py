0efb6984b62a1d110bc85b7af58741300ef15310
mnxx <roth.man@web.de>
2017-06-19 18:01:22 +0200

    Add non-functional basic proposed detector.

--- proposed_detector.py
+++ proposed_detector.py
@@ -3,8 +3,8 @@
 # Author: Manuel Roth.
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# distributed under the GNU General Public License v3.0 is distributed onan "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
@@ -20,11 +20,47 @@ Therefore following assumptions have been made:
 
 import sys
 import numpy as np
-import scipy.linalg as lin
+#import scipy.linalg as lin
 
 
-def detector():
-    return
+def detector(rxVector, H):
+    # M algorithm: breadth-first search with M survivors.
+    M = 1
+    # Possible symbols: FOR NOW NO ANTENNA INDEX INFORMATION.
+    symbolList = np.array([[1], [-1]])
+    # Vector with the estimated symbols; grows with each iteration.
+    x = []
+    # Vector with lowest estimated metrics for each iteration.
+    eList = []
+    # Repeat detection process K times
+    for step, y in enumerate(rxVector[: 8]):
+        # List of selected metrics.
+        em = []
+        # 
+        for possibleSymbol in symbolList:
+            possibleSymbolVector = list(x)
+            possibleSymbolVector.append(possibleSymbol)
+            xm = np.vstack(np.asarray(possibleSymbolVector))
+            h = H[step][: step + 1]
+            #print(str(h.size) + "  " + str(h.shape))
+            h = h.reshape(1, h.size)
+            # Compute the metrics for each candidate vector.
+
+            """ As Y_x, H_x are 2x1 blocks, the order is wrong! """
+            
+            e_index = sum(eList) + np.linalg.norm(y - h.dot(xm))
+            print(e_index)
+            em.append(e_index)            
+        # Obtain corresponding M candidate vectors.
+        # Keep the M elements with the smallest metrics: M = 1.
+        if em[0] < em[1]:
+            x.append(symbolList[0])
+            eList.append(em[0])
+        else:
+            x.append(symbolList[1])
+            eList.append(em[1])
+    # 
+    return x
 
 def bpsk(symbols_per_frame):
     bpsk_map = np.array([1, -1])
@@ -99,7 +135,7 @@ def main():
     # x: KN_t x 1 // x_k: N_t x 1
     # Signal Vector. No spatial information for now.
     signalVector = np.transpose(bpsk(K))
-    #print(signalVector)
+    print(signalVector)
     
     # n: (K+P-1)N_r x 1
     # Noise Vector. Elements are complex Gaussian distributed.
@@ -108,7 +144,11 @@ def main():
     
     # Y: (K+P-1)N_r x 1
     rxVector = channelMatrix.dot(signalVector) + noiseVector
-    print(rxVector)
+    #print(rxVector)
+
+    # DETECT.
+    estimatedVector = detector(rxVector, channelMatrix)
+    print(np.asarray(estimatedVector))
     
 
 if __name__ == '__main__':
