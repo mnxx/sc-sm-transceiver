97303addf8b108ed0647089dfd63fddc9bc60f80
mnxx <roth.man@web.de>
2017-06-19 12:53:20 +0200

    Complete Channel Simulation & Add Channel Matrix Example & Add .gitignore.

--- proposed_detector.py
+++ proposed_detector.py
@@ -26,13 +26,15 @@ import scipy.linalg as lin
 def detector():
     return
 
-
 def bpsk(symbols_per_frame):
     bpsk_map = np.array([1, -1])
     #symbol_indices_train = np.random.randint(0, bpsk_map.size, symbols_per_frame)
-    symbol_indices_data = np.random.randint(0, bpsk_map.size, symbols_per_frame)
+    symbol_indices_data = np.random.randint(0, bpsk_map.size, symbols_per_frame) 
     #s_train = bpsk_map[symbol_indices_train]
-    return bpsk_map[symbol_indices_data]
+    return np.array([bpsk_map[symbol_indices_data]])
+
+def noise(elements_per_frame):
+    return np.random.randn(elements_per_frame, 1) + 1j * np.random.randn(elements_per_frame, 1)
 
 
 def main():
@@ -56,7 +58,7 @@ def main():
     #CN_mean = [0, 0]
     #CN_cov = [[1, 0], [0, 1]]
 
-    OM = np.array([[0 + 0j], [0 + 0j]])
+    #OM = np.array([[0 + 0j], [0 + 0j]])
     #print(OM)
     #x = np.random.multivariate_normal(CN_mean, CN_cov, 1)
     H_0 = np.random.randn(2,1) + 1j * np.random.randn(2,1)
@@ -76,38 +78,37 @@ def main():
     #print(channelMatrix[1])
     #print(channelMatrix[2])
 
-    inputs = (H_0, H_1, H_2, OM)
-
-    input_len = len(inputs)
-    number_rows = 10 #input_len * 2 - 1
-    m, n = H_0.shape
-    print(input_len)
-    print(number_rows)
-    print(m)
-    print(n)
-    channelMatrix = np.zeros((number_rows, m, input_len, n), dtype = H_0.dtype)
+    # H: (K+P-1)N_r x KN_t
+    # General Channel Matrix containing the submatrices of the channel.
+    inputs = (H_0, H_1, H_2)
+    # Number of columns is KN_t = input channel matrices + C.
+    number_columns = len(inputs) + 5
+    # Number of rows is (K+P-1)N_r.
+    number_rows = 10
+    # All submatrices should have the same dimensions.
+    nb_sub_rows, nb_sub_columns = H_0.shape
+    # Create 4-dimensional matrix using the submatrices.
+    channelMatrix = np.zeros((number_rows, nb_sub_rows, number_columns, nb_sub_columns), dtype = H_0.dtype)
     for index, subMatrix in enumerate(inputs):
-        for element in range(input_len):
+        for element in range(number_columns):
             channelMatrix[index + element, : , element, :] = subMatrix
-
-    channelMatrix.shape = (number_rows * m, input_len * n)
-
-    print(channelMatrix)
-        
+    # Flatten the 4-dimensional matrix.
+    channelMatrix.shape = (number_rows * nb_sub_rows, number_columns * nb_sub_columns)
+    #print(channelMatrix.size)
 
     # x: KN_t x 1 // x_k: N_t x 1
     # Signal Vector. No spatial information for now.
-    signalVector = bpsk(K)
+    signalVector = np.transpose(bpsk(K))
     #print(signalVector)
     
     # n: (K+P-1)N_r x 1
     # Noise Vector. Elements are complex Gaussian distributed.
-    #noiseVector = 
+    noiseVector = noise((K+P-1)*N_r)
     #print(noiseVector)
     
     # Y: (K+P-1)N_r x 1
-    #rxMatrix = channelMatrix * signalVector + noiseVector
-
+    rxVector = channelMatrix.dot(signalVector) + noiseVector
+    print(rxVector)
     
 
 if __name__ == '__main__':
