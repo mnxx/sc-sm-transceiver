07749d113e7441b02c3d4f110ca31d42d8ef88a0
mnxx <roth.man@web.de>
2017-06-29 18:34:04 +0200

    Detector working for 2x2 transmission.

--- proposed_detector.py
+++ proposed_detector.py
@@ -3,7 +3,7 @@
 # Author: Manuel Roth.
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the GNU General Public License v3.0 is distributed onan "AS IS"
+# distributed under the GNU General Public License v3.0 is distributed on an "AS IS"
 # BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -12,7 +12,6 @@
 Implementation of ""A Low-Complexity Detection Scheme for Generalized Spatial Modulation Aided Single Carrier Systems" in context of SC SM-MIMO LSA system.
 
 Therefore following assumptions have been made:
- - N_t is 1
  - Frequency selective fading is at hand
 
 """
@@ -23,11 +22,11 @@ import numpy as np
 #import scipy.linalg as lin
 
 
-def detector(rxVector, H):
+def detector(rxVector, H, n_rows, n_columns):
     # M algorithm: breadth-first search with M survivors.
     M = 1
-    # Possible symbols: FOR NOW NO ANTENNA INDEX INFORMATION.
-    symbolList = np.array([[1], [-1]])
+    # Possible symbols: FOR NOW, LIMITED ANTENNA INDEX INFORMATION FOR 2x2.
+    symbolList = np.array([[1, 1], [1, -1], [-1, 1], [-1, -1]])
     # Vector with the estimated symbols; grows with each iteration.
     x = []
     # Vector with lowest estimated metrics for each iteration.
@@ -40,29 +39,28 @@ def detector(rxVector, H):
         for possibleSymbol in symbolList:
             possibleSymbolVector = list(x)
             possibleSymbolVector.append(possibleSymbol)
-            xm = np.vstack(np.asarray(possibleSymbolVector))
-            # As all H_x block-submatrices are 2x1, 2 rows are needed - ADAPT FOR NxM!
-            h = H[2 * step : 2 * step + 2, : step + 1]
+            #xm = np.hstack(np.asarray(possibleSymbolVector))
+            xm = np.asarray(possibleSymbolVector)
+            xm = np.reshape(xm, (n_columns * (step + 1), 1))
+            #print(xm.size)
+            # As all H_x block-submatrices are n_rows x n_columns.
+            h = H[n_rows * step : n_rows * step + n_rows, : n_columns * step + n_columns]
             #print(h)
             #print(str(h.size) + "  " + str(h.shape))
             #h = h.reshape(1, h.size)
             # Compute the metrics for each candidate vector.
-
-            """ As Y_x, H_x are 2x1 blocks, the order is wrong! """
-            
-            e_index = sum(eList) + np.linalg.norm(rxVector[2 * step : 2 * step + 2] - h.dot(xm))
+            #print(np.transpose(xm))
+            #xm = np.transpose(xm)
+            e_index = sum(eList) + np.linalg.norm(rxVector[n_rows * step : n_rows * step + n_rows] - h.dot(xm))
             #print(e_index)
             em.append(e_index)            
         # Obtain corresponding M candidate vectors.
         # Keep the M elements with the smallest metrics: M = 1.
-        if em[0] < em[1]:
-            x.append(symbolList[0])
-            eList.append(em[0])
-        else:
-            x.append(symbolList[1])
-            eList.append(em[1])
-    # 
-    return x
+        best_metric_index = em.index(min(em))
+        x.append(symbolList[best_metric_index])
+        eList.append(em[best_metric_index])
+    # Return the Vector of the K symbols with the best metrics.
+    return np.reshape(x, (16,1))
 
 def bpsk(symbols_per_frame):
     bpsk_map = np.array([1, -1])
@@ -153,7 +151,7 @@ def main():
     # Transpose 
     signalVector = np.transpose(np.array([signalList]))
     prefixedSignalVector = np.transpose(np.array([prefixedSignalList]))
-    print(signalVector)
+    #print(signalVector)
     
     # n: (K+P-1)N_r x 1
     # Noise Vector. Elements are complex Gaussian distributed.
@@ -165,12 +163,12 @@ def main():
     #print(rxVector.size)
 
     # DETECT.
-    estimatedVector = detector(rxVector, channelMatrix)
+    estimatedVector = detector(rxVector, channelMatrix, N_r, N_t)
     #print(estimatedVector)
-    print(np.asarray(estimatedVector))
+    #print(np.asarray(estimatedVector))
 
     # Show if any errors have occured.
-    estimatedVector = np.asarray(estimatedVector)
+    #estimatedVector = np.asarray(estimatedVector)
     print(np.array_equal(signalVector, estimatedVector))
     
 
