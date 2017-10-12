f834ac75fddcc2101d752602679248cea4452a2c
mnxx <roth.man@web.de>
2017-10-12 19:50:41 +0200

    Add QPSK-modulation. Also add modulation index. Needs to be tested.

--- modulation.py
+++ modulation.py
@@ -10,15 +10,23 @@
 """ Implementation of different linear modulation schemes. """
 
 
+import numpy as np
+
+
 class Modulation:
     """ Super class for the modulation schemes. """
 
     def __init__(self):
         self.symbol_list = []
+        self._index = 0
 
     def get_symbols(self):
         """ Return the corresponding symbols of a modulation scheme. """
         return self.symbol_list
+
+    def get_modulation_index(self):
+        """ Return the corresponding modulation index of the modulation. """
+        return self._index
 
 
 class BPSK(Modulation):
@@ -26,6 +34,7 @@ class BPSK(Modulation):
 
     def __init__(self):
         self.symbol_list = [[1], [-1]]
+        self._index = 1
 
     def modulate(self, blocks):
         """ Modulate blocks of bits to symbols. """
@@ -37,7 +46,7 @@ class BPSK(Modulation):
         return blocks
 
     def demodulate(self, symbol):
-        """ Demodulate blocks of symbols to bits. """
+        """ Demodulate symbol to bits. """
         if symbol == -1:
             return 0
         else:
@@ -51,3 +60,53 @@ class BPSK(Modulation):
             else:
                 symbols[index] = 1
         return symbols
+
+
+class QPSK(Modulation):
+    """ Implementation of the QPSK modulation scheme. """
+
+    def __init__(self):
+        self.symbol_list = [[np.exp(1j * np.pi / 4)], [np.exp(3j * np.pi / 4)],
+                            [np.exp(-3j * np.pi / 4)], [np.exp(-1j * np.pi / 4)]]
+        self._index = 2
+
+    def modulate(self, blocks):
+        """ Modulate blocks of bits to symbols. """
+        try:
+            for index, block in enumerate(blocks):
+                if block == [0, 0]:
+                    blocks[index] = np.exp(1j * np.pi / 4)
+                elif block == [0, 1]:
+                    blocks[index] = np.exp(3j * np.pi / 4)
+                elif block == [1, 1]:
+                    blocks[index] = np.exp(-3j * np.pi / 4)
+                elif block == [1, 0]:
+                    blocks[index] = np.exp(-1j * np.pi / 4)
+                else:
+                    raise Exception
+        except Exception:
+            print("Block did not match bit criteria: " + str(block))
+        return blocks
+
+    def demodulate(self, symbol):
+        """ Demodulate symbol to bits. """
+        try:
+            if symbol.real >= 0 and symbol.imag >= 0:
+                return [0, 0]
+            elif symbol.real < 0 and symbol.imag >= 0:
+                return [0, 1]
+            elif symbol.real < 0 and symbol.imag < 0:
+                return [1, 1]
+            elif symbol.real >= 0 and symbol.imag < 0:
+                return [1, 0]
+            else:
+                raise Exception
+        except Exception:
+            print("Symbol did not match QPSK-symbol criteria: " + str(symbol))
+
+    def demodulate_list(self, symbols):
+        """ Demodulate blocks of symbols to bits. """
+        bit_list = []
+        for symbol in symbols:
+            bit_list.append(self.demodulate(symbol))
+        return bit_list
